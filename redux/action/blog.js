84d9e8267dee8ea8e735b772155240aff88f207f
Bhavik Savani <savanibhavik5@gmail.com>
2023-06-14 11:25:28 +0530

    blog app 0.2

--- redux/action/blog.js
+++ redux/action/blog.js
@@ -1,18 +1,12 @@
 export const GETBLOG = "GETBLOG";
 export const getBlog = () => {
   return async (dispatch) => {
-  //  await fetch("http://192.168.29.46:1234/blog").then((res) => {
-  //     dispatch({
-  //       type: GETBLOG,
-  //       data: res.json(),
-  //     });
-  //   });
-  const response = await fetch("http://192.168.29.46:1234/blog")
+    const response = await fetch("http://192.168.29.46:1234/blog");
     const data = await response.json();
-    
+
     dispatch({
       type: GETBLOG,
-      data : data
-    })
+      data: data,
+    });
   };
 };
